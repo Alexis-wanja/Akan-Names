7cad3bd0a76232027cfc544d606b0f746fa4cded
Alexis Wanja <alexis.wanja@student.moringaschool.com>
2021-12-12 15:05:44 +0300

    all commits

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-
-function day (cc,yy,mm,dd){
-    return ((((cc/19)+1*cc-1)+((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7)+1
+function getAkanName() {
+    document.querySelector(".box") 
 }
-let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    
 
-let maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-let femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+   
+   
+
